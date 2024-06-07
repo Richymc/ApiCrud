3ebfcb7d62d8228edc685b37c8e564afc8941477
RichyCordova <ricardo.montoya.c@gmail.com>
2024-06-07 14:43:19 -0700

    Add Variables in to UserModel

--- src/main/java/com/api/crud/models/UserModel.java
+++ src/main/java/com/api/crud/models/UserModel.java
@@ -1,6 +1,25 @@
 package com.api.crud.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name= "user")
+@Getter
+@Setter
 public class UserModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
 }
