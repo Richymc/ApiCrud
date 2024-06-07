ea3966abd039e370e2b61470d12aacf8ba2e6891
RichyCordova <ricardo.montoya.c@gmail.com>
2024-06-07 15:39:08 -0700

    Add getUserById to UserController and UserService

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -23,5 +24,11 @@ public class UserController {
     public UserModel saveUser(@RequestBody UserModel user){
         return this.userService.saveUser(user);
     }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable("id") Long id){
+        return this.userService.getById(id);
+    }
+
 
 }
--- src/main/java/com/api/crud/services/UserService.java
+++ src/main/java/com/api/crud/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +22,8 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
 
 }
