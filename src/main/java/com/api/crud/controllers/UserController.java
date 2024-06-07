3623929977bb686163fbdcc78ccf72e7d7ef3024
RichyCordova <ricardo.montoya.c@gmail.com>
2024-06-07 15:04:46 -0700

    Add saveUser to Controller and service

--- src/main/java/com/api/crud/controllers/UserController.java
+++ src/main/java/com/api/crud/controllers/UserController.java
@@ -3,9 +3,7 @@ package com.api.crud.controllers;
 import com.api.crud.models.UserModel;
 import com.api.crud.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -21,6 +19,9 @@ public class UserController {
         return this.userService.getUsers();
     }
 
-
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
 
 }
